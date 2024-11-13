292ae83877220bec9c1b083e9a0b2ef725242c34
KamilDoniek <kamil.doniek@microsoft.wsei.edu.pl>
2024-11-13 19:48:50 +0100

    Task 2 and 3

--- src/components/PersonalInfo.jsx
+++ src/components/PersonalInfo.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
@@ -5,6 +6,17 @@ import ListGroup from 'react-bootstrap/ListGroup';
 
 
 function PersonInfo ({id, name, birth, eyes}) {
+    const [rating,setRating] = useState(0);
+
+    const handleRate = () => {
+      if (rating === 10) {
+        setRating(1);
+      } else if (rating === 0) {
+        setRating(10); 
+      } else {
+        setRating(rating + 1); 
+      }
+    }
     return (
       <Card style={{ width: '18rem' }}>
       <Card.Body>
@@ -13,8 +25,22 @@ function PersonInfo ({id, name, birth, eyes}) {
                 <ListGroup.Item>Id: {id}</ListGroup.Item>
                 <ListGroup.Item>Data urodzin: {birth}</ListGroup.Item>
                 <ListGroup.Item>Kolor oczu: {eyes}</ListGroup.Item>
+                <ListGroup.Item>rating {rating}</ListGroup.Item>
+
       </ListGroup>
-        <Button variant="primary">Details</Button>
+        <Button variant="primary" onClick={handleRate}>
+          rate
+          </Button>
+          <Button variant="primary">
+          Edit
+          </Button>
+        <Button 
+            variant="danger"
+           
+            className="ms-1">
+          Delete
+        </Button>
+
       </Card.Body>
     </Card>
     );
